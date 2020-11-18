74f57abb098f84ee008eb93004f8173ac2f38b1c
XinyeBai4 <53057203+XinyeBai4@users.noreply.github.com>
2020-11-18 11:33:51 -0500

    Update VolumeWidgetsDisplay.py

--- VolumeWidgetsDisplay.py
+++ VolumeWidgetsDisplay.py
@@ -13,113 +13,215 @@ from PySide2.QtWidgets import QApplication
 from abc import ABCMeta, abstractmethod
 from vtk.qt.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
 
-
 class VolumeWidget():
 
-	def __init__(self, orientation, sliceIndexPercent):
-		if (orientation in ['x', 'y', 'z']):
-			self.orientation = orientation
-		else:
-			self.orientation = 'x'
+    def __init__(self, sliceXIndexPercent, sliceYIndexPercent, sliceZIndexPercent):
 
-		if (sliceIndexPercent > 1 or sliceIndexPercent < 0):
-			self.sliceIndexPercent = 0.5
-		else:
-			self.sliceIndexPercent = sliceIndexPercent
+        if (sliceXIndexPercent < 0 or sliceXIndexPercent > 1):
+            self.sliceXIndexPercent = 0.5
+        else:
+            self.sliceXIndexPercent = sliceXIndexPercent
 
-		self.ren = vtk.vtkRenderer()
-		self.renWin = vtk.vtkRenderWindow()
-		self.iren = vtk.vtkRenderWindowInteractor()
-		self.renWin.AddRenderer(self.ren)
-		self.iren.SetRenderWindow(self.renWin)
+        if (sliceYIndexPercent < 0 or sliceYIndexPercent > 1):
+            self.sliceYIndexPercent = 0.5
+        else:
+            self.sliceYIndexPercent = sliceYIndexPercent
 
-	def render(self, datapath):    
-		# Read DICOM images
-		datapath = 'E:/COVID_CHEST/A034518/4'
-		reader = vtk.vtkDICOMImageReader()
-		reader.SetDirectoryName(datapath)
-		reader.Update()
-
-		outline = vtk.vtkOutlineFilter()
-		outline.SetInputConnection(reader.GetOutputPort())
-		
-		self.mapper = vtk.vtkPolyDataMapper()
-		self.mapper.SetInputConnection(outline.GetOutputPort())
-		
-		actor = vtk.vtkActor()
-		actor.SetMapper(self.mapper)
-		self.style = vtk.vtkInteractorStyleTerrain()
-		self.iren.SetInteractorStyle(self.style)
-		self.iren.SetRenderWindow(self.renWin)
-
-		picker = vtk.vtkCellPicker()
-		picker.SetTolerance(0.005)
-
-		planeWidget = vtk.vtkImagePlaneWidget()
-		planeWidget.SetInteractor(self.iren)
-		planeWidget.SetMarginSizeX(0.0)
-		planeWidget.SetMarginSizeY(0.0)
-			
-		xMin, xMax, yMin, yMax, zMin, zMax = reader.GetExecutive().GetWholeExtent(reader.GetOutputInformation(0))
-		if (self.orientation == 'x'):
-			self.sliceIndex = int(self.sliceIndexPercent * (xMax - xMin) + xMin)
-			planeWidget.SetPlaneOrientationToXAxes()
-		elif (self.orientation == 'y'):
-			self.sliceIndex = int(self.sliceIndexPercent * (yMax - yMin) + yMin)
-			planeWidget.SetPlaneOrientationToYAxes()
-		elif (self.orientation == 'z'):
-			self.sliceIndex = int(self.sliceIndexPercent * (zMax - zMin) + zMin)
-			planeWidget.SetPlaneOrientationToZAxes()
-		else:
-			pass
-		print("You are looking slice {} on {}-axis".format(self.sliceIndex, self.orientation))
-
-		
-		# planeWidget.SetKeyPressActivationValue(self.orientation)
-		planeWidget.SetPicker(picker)
-		planeWidget.RestrictPlaneToVolumeOn()
-		planeWidget.GetPlaneProperty().SetColor(0.0, 0.0, 1.0)
-		planeWidget.DisplayTextOn()
-		planeWidget.UpdatePlacement()
-		planeWidget.TextureInterpolateOff()
-		planeWidget.SetResliceInterpolateToLinear()
-		planeWidget.SetInputData(reader.GetOutput())
-		planeWidget.SetSliceIndex(self.sliceIndex)
-		planeWidget.GetTexturePlaneProperty().SetOpacity(1)
-		planeWidget.On()
-
-		self.ren.AddActor(actor)
+        if (sliceZIndexPercent < 0 or sliceZIndexPercent > 1):
+            self.sliceZIndexPercent = 0.5
+        else:
+            self.sliceZIndexPercent = sliceZIndexPercent
+       
 
 
-		colorMap = vtk.vtkImageMapToColors()
-		colorMap.PassAlphaToOutputOff() # use in RGBA
-		colorMap.SetActiveComponent(0)
-		colorMap.SetOutputFormatToLuminance()
-		colorMap.SetInputData(planeWidget.GetResliceOutput())
-		colorMap.SetLookupTable(planeWidget.GetLookupTable())
-		actor1 = vtk.vtkImageActor()
-		actor1.PickableOff()
-		actor1.SetInputData(colorMap.GetOutput())
-		self.ren.AddActor(actor1)
+        self.ren = vtk.vtkRenderer()
+        self.renWin = vtk.vtkRenderWindow()
+        self.iren = vtk.vtkRenderWindowInteractor()
+        self.renWin.AddRenderer(self.ren)
+        self.iren.SetRenderWindow(self.renWin)
 
-		self.iren.Initialize()
-		self.iren.Start()
-		self.renWin.Render()
+    def render(self, datapath):    
+        # Read DICOM images
+        datapath = 'E:/COVID_CHEST/A034518/4'
+        reader = vtk.vtkDICOMImageReader()
+        reader.SetDirectoryName(datapath)
+        reader.Update()
+
+        xMin, xMax, yMin, yMax, zMin, zMax = reader.GetExecutive().GetWholeExtent(reader.GetOutputInformation(0))
+        self.sliceXIndex = int((xMax - xMin) * self.sliceXIndexPercent + xMin)
+        self.sliceYIndex = int((yMax - yMin) * self.sliceYIndexPercent + yMin)
+        self.sliceZIndex = int((zMax - zMin) * self.sliceZIndexPercent + zMin)
+
+        outline = vtk.vtkOutlineFilter()
+        outline.SetInputConnection(reader.GetOutputPort())
+        
+        self.mapper = vtk.vtkPolyDataMapper()
+        self.mapper.SetInputConnection(outline.GetOutputPort())
+        
+        actor = vtk.vtkActor()
+        actor.SetMapper(self.mapper)
+        self.style = vtk.vtkInteractorStyleTerrain()
+        self.iren.SetInteractorStyle(self.style)
+        self.iren.SetRenderWindow(self.renWin)
+
+        picker = vtk.vtkCellPicker()
+        picker.SetTolerance(0.005)
+
+        planeWidgetX = vtk.vtkImagePlaneWidget()
+        planeWidgetX.SetInteractor(self.iren)
+        planeWidgetX.SetMarginSizeX(0.0)
+        planeWidgetX.SetMarginSizeY(0.0)
+        planeWidgetX.SetPicker(picker)
+        planeWidgetX.RestrictPlaneToVolumeOn()
+        planeWidgetX.GetPlaneProperty().SetColor(0.0, 0.0, 1.0)
+        planeWidgetX.DisplayTextOn()
+        planeWidgetX.UpdatePlacement()
+        planeWidgetX.TextureInterpolateOff()
+        planeWidgetX.SetResliceInterpolateToLinear()
+        planeWidgetX.SetInputData(reader.GetOutput())
+        planeWidgetX.SetPlaneOrientationToXAxes()
+        planeWidgetX.SetSliceIndex(self.sliceXIndex)
+        planeWidgetX.GetTexturePlaneProperty().SetOpacity(1)
+        planeWidgetX.On()
+
+        planeWidgetY = vtk.vtkImagePlaneWidget()
+        planeWidgetY.SetInteractor(self.iren)
+        planeWidgetY.SetMarginSizeX(0.0)
+        planeWidgetY.SetMarginSizeY(0.0)
+        planeWidgetY.SetPicker(picker)
+        planeWidgetY.RestrictPlaneToVolumeOn()
+        planeWidgetY.GetPlaneProperty().SetColor(1.0, 0.0, 0.0)
+        planeWidgetY.DisplayTextOn()
+        planeWidgetY.UpdatePlacement()
+        planeWidgetY.TextureInterpolateOff()
+        planeWidgetY.SetResliceInterpolateToLinear()
+        planeWidgetY.SetInputData(reader.GetOutput())
+        planeWidgetY.SetPlaneOrientationToYAxes()
+        planeWidgetY.SetSliceIndex(self.sliceYIndex)
+        planeWidgetY.GetTexturePlaneProperty().SetOpacity(1)
+        planeWidgetY.On()
+
+        planeWidgetZ = vtk.vtkImagePlaneWidget()
+        planeWidgetZ.SetInteractor(self.iren)
+        planeWidgetZ.SetMarginSizeX(0.0)
+        planeWidgetZ.SetMarginSizeY(0.0)
+        planeWidgetZ.SetPlaneOrientationToZAxes()
+        planeWidgetZ.SetPicker(picker)
+        planeWidgetZ.RestrictPlaneToVolumeOn()
+        planeWidgetZ.GetPlaneProperty().SetColor(0.0, 1.0, 0.0)
+        planeWidgetZ.DisplayTextOn()
+        planeWidgetZ.UpdatePlacement()
+        planeWidgetZ.TextureInterpolateOff()
+        planeWidgetZ.SetResliceInterpolateToLinear()
+        planeWidgetZ.SetInputData(reader.GetOutput())
+        planeWidgetZ.SetSliceIndex(self.sliceZIndex)
+        planeWidgetZ.GetTexturePlaneProperty().SetOpacity(1)
+        planeWidgetZ.On()
+
+        self.ren.AddActor(actor)
+
+        ren1 = vtk.vtkRenderer()
+        self.renWin.AddRenderer(ren1)
+        self.iren.SetRenderWindow(self.renWin)
+        
+
+        planeWidgetX1 = vtk.vtkImagePlaneWidget()
+        planeWidgetX1.SetInteractor(self.iren)
+        planeWidgetX1.SetMarginSizeX(0.0)
+        planeWidgetX1.SetMarginSizeY(0.0)
+        planeWidgetX1.SetPicker(picker)
+        planeWidgetX1.RestrictPlaneToVolumeOn()
+        planeWidgetX1.GetPlaneProperty().SetColor(0.0, 0.0, 1.0)
+        planeWidgetX1.DisplayTextOff()
+        planeWidgetX1.TextureInterpolateOff()
+        planeWidgetX1.SetResliceInterpolateToLinear()
+        planeWidgetX1.SetInputData(reader.GetOutput())
+        planeWidgetX1.SetPlaneOrientationToXAxes()
+        planeWidgetX1.SetSliceIndex(self.sliceXIndex)
+        planeWidgetX1.GetTexturePlaneProperty().SetOpacity(1)
+        planeWidgetX1.On()
+
+        ren1.AddActor(actor)
+
+
+
+        ren2 = vtk.vtkRenderer()
+        self.renWin.AddRenderer(ren2)
+        self.iren.SetRenderWindow(self.renWin)
+        
+        
+        planeWidgetY1 = vtk.vtkImagePlaneWidget()
+        planeWidgetY1.SetInteractor(self.iren)
+        planeWidgetY1.SetMarginSizeX(0.0)
+        planeWidgetY1.SetMarginSizeY(0.0)
+        planeWidgetY1.SetPicker(picker)
+        planeWidgetY1.RestrictPlaneToVolumeOn()
+        planeWidgetY1.GetPlaneProperty().SetColor(1.0, 0.0, 0.0)
+        planeWidgetY1.DisplayTextOn()
+        planeWidgetY1.TextureInterpolateOff()
+        planeWidgetY1.SetResliceInterpolateToLinear()
+        planeWidgetY1.SetInputData(reader.GetOutput())
+        planeWidgetY1.SetPlaneOrientationToYAxes()
+        planeWidgetY1.SetSliceIndex(self.sliceYIndex)
+        planeWidgetY1.GetTexturePlaneProperty().SetOpacity(1)
+        planeWidgetY1.On()
+
+        ren2.AddActor(actor)
+
+
+        ren3 = vtk.vtkRenderer()
+        self.renWin.AddRenderer(ren3)
+        self.iren.SetRenderWindow(self.renWin)
+        
+
+        planeWidgetZ1 = vtk.vtkImagePlaneWidget()
+        planeWidgetZ1.SetInteractor(self.iren)
+        planeWidgetZ1.SetMarginSizeX(0.0)
+        planeWidgetZ1.SetMarginSizeY(0.0)
+        planeWidgetZ1.SetPlaneOrientationToZAxes()
+        planeWidgetZ1.SetPicker(picker)
+        planeWidgetZ1.RestrictPlaneToVolumeOn()
+        planeWidgetZ1.GetPlaneProperty().SetColor(0.0, 1.0, 0.0)
+        planeWidgetZ1.DisplayTextOn()
+        planeWidgetZ1.TextureInterpolateOff()
+        planeWidgetZ1.SetResliceInterpolateToLinear()
+        planeWidgetZ1.SetInputData(reader.GetOutput())
+        planeWidgetZ1.SetSliceIndex(self.sliceZIndex)
+        planeWidgetZ1.GetTexturePlaneProperty().SetOpacity(1)
+        planeWidgetZ1.On()
+
+        ren3.AddActor(actor)
+
+
+        ren1.SetBackground(1.0, 0.0, 0.0)
+        ren2.SetBackground(0.0, 1.0, 0.0)
+        ren3.SetBackground(0.0, 0.0, 1.0)
+        
+        self.renWin.SetSize(600,400)
+        self.ren.SetViewport(0, 0.5, 0.5, 1)
+        ren1.SetViewport(0.5, 0.5, 1, 1)
+        ren2.SetViewport(0, 0, 0.5 , 0.5)
+        ren3.SetViewport(0.5, 0 ,1, 0.5)
+        
+
+        self.iren.Initialize()
+        self.iren.Start()
+        self.renWin.Render()
 
 
 
 
 if __name__ == '__main__':
-	dirname = os.path.dirname(PySide2.__file__)
-	plugin_path = os.path.join(dirname, 'plugins', 'platforms')
-	os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = plugin_path
+    dirname = os.path.dirname(PySide2.__file__)
+    plugin_path = os.path.join(dirname, 'plugins', 'platforms')
+    os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = plugin_path
 
-	# Create an application
-	app = QApplication(sys.argv)
+    # Create an application
+    app = QApplication(sys.argv)
 
-	# Add view to mainwindow
-	view1 = VolumeWidget('z', 0.4)
-	view1.render('E:/COVID_CHEST/A034518/4')
+    # Add view to mainwindow
+    view1 = VolumeWidget(0.5, 0.7, 0.2)
+    view1.render('E:/COVID_CHEST/A034518/4')
 
-	sys.exit(app.exec_())
-	
+    sys.exit(app.exec_())
+    
